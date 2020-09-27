39c3a9867a1d8a19dda6b0cdd7557b1efc744196
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-27 18:03:28 -0300

    validate bid amount

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -4,29 +4,37 @@ import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function getAuction(event, context) {
-  try {
-    const { id } = event.pathParameters;
+export async function getAuctionById(id) {
+  let auction;
 
+  try {
     const result = await dynamodb.get({
       TableName: process.env.AUCTIONS_TABLE_NAME,
       Key: { id }
     }).promise();
 
-    const auctions = result?.Item;
-
-    if (!auctions) {
-      throw new createError.NotFound(`Auction with ${id} not found!`);
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(auctions),
-    };
+    auction = result.Item;
   } catch (error) {
     console.log(error);
     throw new createError.InternalServerError(error);
   }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with ${id} not found!`);
+  }
+
+  return auction;
+};
+
+async function getAuction(event, context) {
+  const { id } = event.pathParameters;
+
+  const auction = await getAuctionById(id);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
 };
 
 export const handler = commonMiddleware(getAuction);
