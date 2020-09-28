b87cf7363feb419adc1e6ae37980df31db4602d3
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-28 12:05:22 -0300

    get auctions by status

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -5,10 +5,22 @@ import createError from 'http-errors';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status'
+    },
+  };
+
   try {
-    const result = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME
-    }).promise();
+    const result = await dynamodb.query(params).promise();
 
     const auctions = result.Items;
 
